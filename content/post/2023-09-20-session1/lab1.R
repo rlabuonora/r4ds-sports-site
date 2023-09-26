6e612f680bafb16b6642173c2fb8df62c156e3fd
Rafael La Buonora <info@rlabuonora.com>
2023-09-26 17:38:37 -0300

    Finish session 2

--- content/post/2023-09-20-session1/lab1.R
+++ content/post/2023-09-20-session1/lab1.R
@@ -9,7 +9,7 @@ add <- readRDS('./add.rds')
 
 df <- left_join(box, add, by="Team")
 
-df_grouped <- group_by(t, Playoff)
+df_grouped <- group_by(df, Playoff)
 
 tbl <- summarize(df_grouped,
             Steals=mean(STL), 
