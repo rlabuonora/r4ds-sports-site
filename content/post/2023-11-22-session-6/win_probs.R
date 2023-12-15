d5c9be61350ba8880ac05876d5720717baba097f
Rafael La Buonora <info@rlabuonora.com>
2023-12-15 06:58:59 -0300

    Update

--- content/post/2023-11-22-session-6/win_probs.R
+++ content/post/2023-11-22-session-6/win_probs.R
@@ -1,6 +1,10 @@
 library(espnscrapeR)
 library(dplyr)
 
+schedule_18 <- espnscrapeR::get_nfl_schedule(season='2018')
+game_data <- schedule_18 %>% 
+  filter(game_id=="401030956")
+
 # esto para un left join!
 win_prob <- get_espn_win_prob(game_id = "401030956")
 home_teams <- get_nfl_teams() %>% 
@@ -18,3 +22,20 @@ ggplot(win_probs, aes(row_id, home_win_percentage)) +
   geom_line()
 
 saveRDS(win_probs, '../r4ds-sports/content/post/2023-12-14-session-7/win_probs.rds')
+
+runs_long %>% 
+  filter(name %in% c("heart_rate")) %>% 
+  filter(idx<13) %>% 
+  ggplot(aes(date, value)) + 
+  geom_line(color="blue") +
+  #geom_area(alpha=0.6, fill="blue") +
+  ggplot2::labs(title="Heart Rate during training sessions",
+                subtitle="",
+                x="",
+                y="BPM") + 
+  facet_wrap(~idx
+             , 
+             scales = "free"
+             ) + 
+  theme_minimal()
+
